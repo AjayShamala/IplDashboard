2bdb839eca1c40f5ded4c2e66c8b2fbd38fd2e6e
Shyamala Ajay Kumar <167419255+AjayShamala@users.noreply.github.com>
2024-05-19 18:03:24 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,7 @@
-// Write your code here
+import './index.css'
+const NotFound = () => (
+  <div className="contain">
+    <p className="page">Page Not Found!!</p>
+  </div>
+)
+export default NotFound
